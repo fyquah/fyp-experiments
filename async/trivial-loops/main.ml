6ff2c024a6d7ee8a311e9da1fc75cf5591463f8a
Fu Yong Quah <quah.fy95@gmail.com>
2017-12-05 12:54:36 +0000

    commit

--- async/trivial-loops/main.ml
+++ async/trivial-loops/main.ml
@@ -9,11 +9,22 @@ let run () =
   Deferred.repeat_until_finished i (fun i ->
     if i > 0
     then Deferred.return (`Finished i)
+    else Deferred.return (`Repeat (i + 1)))
+
+  >>= fun n ->
+  Deferred.repeat_until_finished n (fun i ->
+    if i > 0
+    then Deferred.return (`Finished i)
+    else Deferred.return (`Repeat (i + 1)))
+
+  >>= fun n ->
+  Deferred.repeat_until_finished n (fun i ->
+    if i > 0
+    then Deferred.return (`Finished i)
     else Deferred.return (`Repeat (i + 1))
   )
   >>| fun a -> printf "%d\n" (a + 1)
 
 let () =
-  >>=
   ignore (run ());
   never_returns (Scheduler.go ())
